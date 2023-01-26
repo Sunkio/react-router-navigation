d4e1dc9805005a17cd77e14440dc19d4c4cab31f
Tanja Schmidt <tanjadev04@gmail.com>
2023-01-26 12:21:43 +0100

    add personal pics & text

--- blog-site/src/blogs.js
+++ blog-site/src/blogs.js
@@ -2,13 +2,13 @@ export const my_blogs = {
     "blog_1":{
         "title":"My First Computer Program",
         "author":"le stackie",
-        "date":"2021-03-01",
+        "date":"2023-01-20",
         "content":"I started my programming journey when I was in secondary school. Back then, I joined an Infocomm club and I started to learn Scratch, a versatile drag-and-drop tool that teaches me simple logic flow, data conditioning and simple iteration loops. It got me so interested in programming that I spent hours making a flight simulator on Scratch.\n\nI was so into programming that I took the computing class for my GCE O Levels. There, I started learning my first real programming language and that was Python. I was learning data structuring, the basics of networking, functions and classes and much more. My computing teacher gave my classes a series of programming questions in a gamified manner. Each time we got an algorithm correct, we were awarded points. My friends and I will spend time after school trying to outdo each other.\n\nWith the experience I had, and the opportunities I was given, I decided to pursue a career in programming. I am currently studying Computer Science at the National University of Singapore. I am currently in my 3rd year and I am looking forward to my final year project and internship opportunities.",
     },
     "blog_2":{
         "title": "My Learning Experience in StackUp",
         "author": "le stackie",
-        "date": "2023-01-26",
-        "content":"tba - 50 words about my learning journey so far on StackUp"
+        "date": "2023-01-20",
+        "content":"I discovered StackUp only a few days ago and signed up wondering what to expect. I like the idea of submitting quests with the chance of winning a small reward. So far, I’ve taken part in 3 quests - the topics were React and Tableau. This React Router Navigation example page will be my fourth quest submission. The quests have been well-structured and fun to do, and I most certainly will take part in more quests in current and future challenges."
     }
 }
--- blog-site/src/pages/AuthorPage.js
+++ blog-site/src/pages/AuthorPage.js
@@ -9,9 +9,9 @@ const AuthorPage = () => {
                 <div className="inner-container">
                     <h1 className="page-title">About the Author: Tanja</h1>
 
-                    <img src="./img/header.jpg" className="hero-image" alt="" />
+                    <img src="./img/author.jpg" className="hero-image" alt="" />
                     <div className="blog-content">
-                        <p className="blog-text">TBA - 50 words or more discussing my interest in web development</p>
+                        <p className="blog-text">Tanja has been interested in programming in general and in web development in particular for a very long time. But it was only six years ago that she started learning HTML, CSS, JavaScript, and jQuery. She took courses but found it hard to build her own projects at that time. Fast forward a few years: Tanja got accepted to a software engineering school and learned programming in C. She got very interested in web3 development and, via this, found her way back to web2 development as well. She finally enjoys programming in JavaScript and exploring the vast ecosystem of libraries, frameworks, and tools that surround it.</p>
                     </div>
                 </div>
             </div>
